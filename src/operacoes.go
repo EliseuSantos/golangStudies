effb71ba4a3b424eb503fc06f2779ccb30605458
Eliseu Santos <s.eliseu.santos@gmail.com>
2018-09-10 15:37:43 -0300

    Aprendendo operações básicas

---
+++ src/operacoes.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	numero1, numero2 := 10, 8
+	fmt.Println("Multiplicação:\n", numero1*numero2)
+	fmt.Println("Divisão:\n", numero1/numero2)
+	fmt.Println("Subtração\n", numero1-numero2)
+}
